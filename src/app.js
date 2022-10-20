8bb969e5896ce5db0b5b45b832c9af1938e1b6dc
Andre Koloda <andreckoloda@gmail.com>
2022-10-20 13:41:05 -0300

    Adicionando as rotas ao iniciar o projeto

--- src/app.js
+++ src/app.js
@@ -2,5 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(require('./routes/acessosRoute'));
+app.use(require('./routes/assembleiaRoute'));
+app.use(require('./routes/condominioRoute'));
+app.use(require('./routes/contratoRoute'));
+app.use(require('./routes/taxaRoute'));
+app.use(require('./routes/usuarioRoute'));
+app.use(require('./routes/vagaRoute'));
+app.use(require('./routes/veiculoRoute'));
+app.use(require('./routes/votacaoRoute'));
 
 module.exports = app;
